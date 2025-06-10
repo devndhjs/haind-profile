a4af847bc28bdfee4e4efd54c470fef33c0803ab
Desmond Nguyen <desmond.nguyen@laidon.com>
2025-06-10 16:34:43 +0700

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,39 @@
 "use client";
 
 import { JSX, useEffect, useState } from "react";
-import ProfilePage from "./profile/page";
 import api from "@/lib/axios";
 
 export default function MainPage(): JSX.Element {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({} as any);
   useEffect(() => {
     const fetchData = async () => {
-      const res = await api.get("/board");
+      const res = await api.get("/board/1");
       setData(res.data);
-      console.log(data);
     };
 
     fetchData();
   }, []);
 
-  return <ProfilePage />;
+  return (
+    <>
+      {data?.lists?.map((e: any) => {
+        return (
+          <div key={e.id}>
+            <div>{e.title}</div>
+            <div className="">
+              {e.cards?.map((item: any) => {
+                return (
+                  <div key={item.id}>
+                    <div>{item.title}</div>
+                    <div>{item.description}</div>
+                    <div>{item.dueDate}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
